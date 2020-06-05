cf0b2761df3bcac666f0e2f3ea48bcae741b15be
mateusmuller <mateusmuller@github.com>
2020-06-05 17:05:17 -0300

    modify ingress

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
-}
-
 resource "aws_key_pair" "k8s-key" {
   key_name   = "k8s-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnWNwGRVEivd269B9B7QZ/g/eRwbwuC+c3GDMNc1Gpmw810hYO9n1iMqR27azxFyjSPIpiADKBf8yI/dt6tkQGW3KzgriBc4T/4qNLVMIQ2fr881N7i3fbsv0VIw+qHqyl2rGgDHhhe7gJBaefItPdEfpGcfVPknckwrumNG1R2+VileiN2it6kKxQRMW6o0HBNuNY738A9nncpgzU3qnFfy9MF5Vs7o3eYaQOKaqbpdVbSD2MLGZrXlnrgY7L3EUOiGJMBy/Bw4p0GhoznakPg1ckFAiXwJw3hr7CLlNOTmoKhd1vo5wEsYpmChTvlHxn7UnbS4UbwblL/5QT44fIt1lgduwmePsZxBDtKzjS4XhII0lud3B6FMqPgsJpyOy6/Bx/QJvi780s2/SlAyIYZlcr6YN65a9hSlqk61KunhjNKMOZz6/rymyeYcZDIxurDUbA21tshm85Z/+6BZiN2PiDYUvJ7p8e/EF47PWvbVIy84kPN70xEFP9kQI5+Gs= mateus@ubuntu-19"
@@ -24,7 +20,7 @@ resource "aws_security_group" "k8s-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
