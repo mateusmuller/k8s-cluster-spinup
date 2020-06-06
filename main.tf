54589fd44bfbaa6cf941cc2abd88c904a1b18514
mateusmuller <mateusmuller@github.com>
2020-06-06 09:26:12 -0300

    modified tags on terraform

--- main.tf
+++ main.tf
@@ -36,9 +36,22 @@ resource "aws_instance" "kubernetes" {
   ami           = "ami-085925f297f89fce1"
   instance_type = "t3.medium"
   key_name = "k8s-key"
-  count = 3
+  count = 2
   tags = {
     name = "k8s"
+    type = "worker"
+  }
+  security_groups = ["${aws_security_group.k8s-sg.name}"]
+}
+
+resource "aws_instance" "kubernetes" {
+  ami           = "ami-085925f297f89fce1"
+  instance_type = "t3.medium"
+  key_name = "k8s-key"
+  count = 1
+  tags = {
+    name = "k8s"
+    type = "master"
   }
   security_groups = ["${aws_security_group.k8s-sg.name}"]
 }
