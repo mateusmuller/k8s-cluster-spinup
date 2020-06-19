0f5de64ad0847787c0f1580f23f7db63568d04fc
mateusmuller <mateusmuller@github.com>
2020-06-18 21:41:41 -0300

    modifieed backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         region = "us-east-1"
-        bucket = "k8s-backend"
+        bucket = "k8s-backend-video"
         encrypt = "true"
         key = "terraform.tfstate"
     }
